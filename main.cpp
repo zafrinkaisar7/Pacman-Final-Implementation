276e57b140438296085a5502a6f3e07f4527d901
kaisarzafrin07 <zafrinkaisar07@gmail.com>
2026-05-18 06:32:19 +0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <GL/glut.h>
+#include <windows.h>
+#include <mmsystem.h>
+
+#pragma comment(lib,"winmm.lib")
+
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const int WIDTH = 600;
+const int HEIGHT = 600;
+
+const int ROWS = 20;
+const int COLS = 20;
+
+const int TILE = WIDTH / COLS;
+
+int maze[ROWS][COLS];
+
+int pacX = 1;
+int pacY = 1;
+
+int dirX = 0;
+int dirY = 0;
+
+int score = 0;
+int lives = 3;
+
+string playerName;
+
+bool inMenu = true;
+bool gameOver = false;
+
+float mouthAngle = 30;
+bool opening = true;
+
+const double PI = 3.141592653589793;
+
